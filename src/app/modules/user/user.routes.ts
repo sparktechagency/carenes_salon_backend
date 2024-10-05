05871b34fb5eda2e8f77966c654045f2736a0b61
manik <maniksarker652@gmail.com>
2024-10-05 18:21:17 +0600

    make some changes

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,9 +1,6 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import validateRequest from '../../middlewares/validateRequest';
-import userValidations from './user.validation';
 import userControllers from './user.controller';
-import auth from '../../middlewares/auth';
-import { USER_ROLE } from './user.constant';
 import customerValidations from '../customer/customer.validation';
 import riderValidations from '../rider/rider.validation';
 import { uploadFile } from '../../helper/fileUploader';
