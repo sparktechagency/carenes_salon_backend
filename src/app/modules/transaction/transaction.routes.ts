8c05d2eb42011da01efe0894a3c662ccf87ed8a6
manik <maniksarker652@gmail.com>
2024-12-02 12:42:14 +0600

    make routes for transactions

---
+++ src/app/modules/transaction/transaction.routes.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
+import TransactionController from './transaction.controller';
+
+const router = express.Router();
+
+router.get(
+  '/get-all-transaction',
+  auth(USER_ROLE.superAdmin),
+  TransactionController.getAllTransaction,
+);
+router.get(
+  '/get-client-transaction',
+  auth(USER_ROLE.client),
+  TransactionController.getClientTransaction,
+);
+
+export const transactionRoutes = router;
