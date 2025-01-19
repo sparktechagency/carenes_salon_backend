962f9b57ddeb75a8c49c8373dbe36db03d6b6ca6
manik <maniksarker652@gmail.com>
2025-01-19 12:43:26 +0600

    make changes in isAccount ready

--- src/app/helper/isAccountReady.ts
+++ src/app/helper/isAccountReady.ts
@@ -5,19 +5,14 @@ import config from '../config';
 const stripe = new Stripe(config.stripe.stripe_secret_key as string);
 const isAccountReady = async (accountId: string): Promise<boolean> => {
   try {
-    // Retrieve connected account details
     const account = await stripe.accounts.retrieve(accountId);
 
-    const { capabilities, requirements } = account;
+    const { capabilities } = account;
 
-    // Check if required capabilities are active
     const isCardPaymentsActive = capabilities?.card_payments === 'active';
     const isTransfersActive = capabilities?.transfers === 'active';
 
-    // Ensure there are no pending requirements
-    const currentlyDue = requirements?.currently_due || [];
-    const isReady =
-      isCardPaymentsActive && isTransfersActive && currentlyDue.length === 0;
+    const isReady = isCardPaymentsActive && isTransfersActive;
 
     return isReady;
   } catch (error: any) {
