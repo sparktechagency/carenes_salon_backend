ddefb3f329c219227547e726294e608988697741
manik <maniksarker652@gmail.com>
2025-01-19 12:25:14 +0600

    setup route for update onboaring link

--- src/app/modules/stripe/stripe.routes.ts
+++ src/app/modules/stripe/stripe.routes.ts
@@ -1,13 +1,20 @@
-import express from "express";
-import auth from "../../middlewares/auth";
-import { USER_ROLE } from "../user/user.constant";
-import stripeController from "./stripe.controller";
-
+import express from 'express';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
+import stripeController from './stripe.controller';
 
 const router = express.Router();
 
+router.post(
+  '/connect-stripe',
+  auth(USER_ROLE.client),
+  stripeController.createOnboardingLink,
+);
 
-router.post("/connect-stripe",auth(USER_ROLE.client),stripeController.createOnboardingLink);
-
+router.post(
+  '/update-connected-account',
+  auth(USER_ROLE.client),
+  stripeController.updateOnboardingLink,
+);
 
 export const stripeRoutes = router;
